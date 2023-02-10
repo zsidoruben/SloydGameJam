513d4cdf66c07c49354d70c94603e0380c26ad0c
Varga Balázs Ádám <112722847+VargaBalazsAdam@users.noreply.github.com>
2023-02-10 14:15:40 +0100

    Map design v1

---
+++ OurProject/Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickeringLight : MonoBehaviour {
+
+	public float minTime = 0.05f;
+	public float maxTime = 1.2f;
+
+	private float timer;
+	private Light l;
+
+	void Start () {
+		l = GetComponent<Light>();
+		timer = Random.Range(minTime, maxTime);
+	}
+	
+	void Update () 
+	{
+		timer -= Time.deltaTime;
+		if(timer <= 0)
+		{
+			l.enabled = !l.enabled;
+			GetComponent<AudioSource>().Play();
+			timer = Random.Range(minTime, maxTime);
+		}
+	}
+}
